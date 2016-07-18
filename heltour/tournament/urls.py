8ea5dde5da30956bf0fc6a78538db28b1c849dd9
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-07-18 01:11:04 -0400

    Starting to work on public views

--- heltour/tournament/urls.py
+++ heltour/tournament/urls.py
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('heltour.tournament.urls')),
+    url(r'^season/(?P<season_id>[0-9]+)/round/(?P<round_number>[0-9]+)/pairings/$', views.pairings, name='pairings'),
 ]
--- heltour/tournament/views.py
+++ heltour/tournament/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def pairings(request, season_id, round_number):
+    context = {
+        'round_number': round_number
+    }
+    return render(request, 'tournament/pairings.html', context)
