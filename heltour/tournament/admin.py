9ce0f59ad288ae7347d258d6c86c7041fba61eb1
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-07-19 14:45:19 -0400

    Move admin actions into classes

--- heltour/tournament/admin.py
+++ heltour/tournament/admin.py
@@ -26,54 +26,46 @@ class SeasonAdmin(VersionAdmin):
     #       all of the round records for this season, and don't let
     #       the number of rounds to change after that.
 
-def generate_pairings(modeladmin, request, queryset):
-    if queryset.count() > 1:
-        modeladmin.message_user(request, 'Pairings can only be generated one round at a time', messages.ERROR)
-        return
-    try:
-        pairinggen.generate_pairings(queryset.first())
-        modeladmin.message_user(request, 'Pairings created', messages.INFO)
-    except ValueError:
-        modeladmin.message_user(request, 'Pairings already exist for the selected round', messages.ERROR)
-
 # TODO: flesh out the rest of these admin classes based on the workflows of
 #       The moderators
 #-------------------------------------------------------------------------------
 @admin.register(models.Round)
 class RoundAdmin(VersionAdmin):
     list_filter = ('season',)
-    actions = [generate_pairings]
+    actions = ['generate_pairings']
+    
+    def generate_pairings(self, request, queryset):
+        if queryset.count() > 1:
+            self.message_user(request, 'Pairings can only be generated one round at a time', messages.ERROR)
+            return
+        try:
+            pairinggen.generate_pairings(queryset.first())
+            self.message_user(request, 'Pairings created', messages.INFO)
+        except ValueError:
+            self.message_user(request, 'Pairings already exist for the selected round', messages.ERROR)
 
 #-------------------------------------------------------------------------------
 @admin.register(models.RoundChange)
 class RoundChangeAdmin(VersionAdmin):
     pass
 
-def update_selected_player_ratings(modeladmin, request, queryset):
-    try:
-        for player in queryset.all():
-            rating, games_played = lichessapi.get_user_classical_rating_and_games_played(player.lichess_username)
-            player.rating = rating
-            player.games_played = games_played
-            player.save()
-        modeladmin.message_user(request, 'Rating(s) updated', messages.INFO)
-    except:
-        modeladmin.message_user(request, 'Error updating rating(s) from lichess API', messages.ERROR)
-
 #-------------------------------------------------------------------------------
 @admin.register(models.Player)
 class PlayerAdmin(VersionAdmin):
     search_fields = ('lichess_username',)
     list_filter = ('is_active',)
-    actions = [update_selected_player_ratings]
-
-def update_board_order_by_rating(modeladmin, request, queryset):
-    for team in queryset.all():
-        members = team.teammember_set.order_by('-player__rating')
-        for i in range(len(members)):
-            members[i].board_number = i + 1
-            members[i].save()
-    modeladmin.message_user(request, 'Board order updated', messages.INFO)
+    actions = ['update_selected_player_ratings']
+    
+    def update_selected_player_ratings(self, request, queryset):
+        try:
+            for player in queryset.all():
+                rating, games_played = lichessapi.get_user_classical_rating_and_games_played(player.lichess_username)
+                player.rating = rating
+                player.games_played = games_played
+                player.save()
+            self.message_user(request, 'Rating(s) updated', messages.INFO)
+        except:
+            self.message_user(request, 'Error updating rating(s) from lichess API', messages.ERROR)
 
 #-------------------------------------------------------------------------------
 @admin.register(models.Team)
@@ -81,7 +73,15 @@ class TeamAdmin(VersionAdmin):
     list_display = ('name', 'season')
     search_fields = ('name',)
     list_filter = ('season',)
-    actions = [update_board_order_by_rating]
+    actions = ['update_board_order_by_rating']
+    
+    def update_board_order_by_rating(self, request, queryset):
+        for team in queryset.all():
+            members = team.teammember_set.order_by('-player__rating')
+            for i in range(len(members)):
+                members[i].board_number = i + 1
+                members[i].save()
+        self.message_user(request, 'Board order updated', messages.INFO)
 
 #-------------------------------------------------------------------------------
 @admin.register(models.TeamMember)
