f2ad8c0a008ee4f8ce480e67150df691d1e1cd84
Lakin Wecker <lakin@structuredabstraction.com>
2016-07-24 17:00:10 -0600

    Appropriate python path

--- sysadmin/run-heltour.sh
+++ sysadmin/run-heltour.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/heltour.lakin.ca/
 export PYTHONPATH=/var/www/heltour.lakin.ca/
-gunicorn -w 4 --pythonpath=/var/www/heltour.lakin.ca/ -b 127.0.0.1:8580  heltour.wsgi:application
+gunicorn -w 4 --pythonpath=/var/www/heltour.lakin.ca/env/lib/python2.7/site-packages/ -b 127.0.0.1:8580  heltour.wsgi:application
 
