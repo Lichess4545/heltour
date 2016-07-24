4b0b6de33e3defb06686fb6e717375317680d88d
Lakin Wecker <lakin@structuredabstraction.com>
2016-07-24 16:52:38 -0600

    Renaming this file so that our rsync filter puts it on the server.

--- sysadmin/update-requirements.sh
+++ sysadmin/update-requirements.sh
@@ -2,6 +2,6 @@
 pushd /var/www/heltour.lakin.ca/
 source /var/www/heltour.lakin.ca/env/bin/activate
 pushd /var/www/heltour.lakin.ca/current/
-/var/www/heltour.lakin.ca/env/bin/pip install -r /var/www/heltour.lakin.ca/current/sysadmin/requirements.txt
+/var/www/heltour.lakin.ca/env/bin/pip install -r /var/www/heltour.lakin.ca/current/sysadmin/heltour-requirements.txt
 popd
 popd
