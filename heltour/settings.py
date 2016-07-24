fe1155b9df41d751fd8b9ece8755aa537b50146e
Lakin Wecker <lakin@structuredabstraction.com>
2016-07-24 16:39:17 -0600

    Further setup in preparation for deployment.
    Includes host-based settings, and importing live settings overrides.

--- heltour/settings.py
+++ heltour/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -23,9 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'gje)lme+inrew)s%@2mvhj+0$vip^n500i22-o23lm$t1)aq8e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'heltour.lakin.ca',
+]
 
 
 # Application definition
@@ -131,8 +132,17 @@ BOOTSTRAP3 = {
     'set_placeholder': False
 }
 
-GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/media/mausoleum/Work/Development/heltour/gspread.conf'
- 
+GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/etc/heltour/gspread.conf'
+
+# Host-based settings overrides.
+import platform
+import re
+try:
+    hostname = platform.node().split('.')[0]
+    exec 'from .local.%s import *' % re.sub('[^\w]', '_', hostname)
+except ImportError:
+    pass # ignore missing local settings
+
 # Allow live settings (which aren't in the repository) to override the development settings.
 import os
 import json
--- heltour/wsgi.py
+++ heltour/wsgi.py
@@ -11,7 +11,8 @@ import os
 
 local_dir = os.path.join(os.path.dirname(__file__))
 activate_this = '/var/www/heltour.lakin.ca/env/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
+if os.path.exists(activate_this):
+    execfile(activate_this, dict(__file__=activate_this))
 
 
 from django.core.wsgi import get_wsgi_application
