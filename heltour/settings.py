bdb035819170fb3d5619a0f4ad87f2920cbddd64
Lakin Wecker <lakin@structuredabstraction.com>
2016-07-26 20:36:02 -0600

    Adding myself as an admin.

--- heltour/settings.py
+++ heltour/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+
+ADMINS = [('Lakin Wecker', 'lakin.wecker@gmail.com'),]
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
