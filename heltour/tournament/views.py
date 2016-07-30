c19ccaeae24a426f83b3470c5bca54054d6097ce
Lakin Wecker <lakin@structuredabstraction.com>
2016-07-29 20:23:01 -0600

    Cleaning up the imports.

--- heltour/tournament/views.py
+++ heltour/tournament/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http.response import Http404
-from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from datetime import timedelta
 from .models import *
