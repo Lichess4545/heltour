bc99386a42e05952660000fc781a3fbe168513ed
Lakin Wecker <lakin@structuredabstraction.com>
2016-08-06 11:24:58 -0600

    ensure stdout makes it to the error log

--- sysadmin/run-heltour.sh
+++ sysadmin/run-heltour.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/heltour.lakin.ca/
 export PYTHONPATH=/var/www/heltour.lakin.ca/
-/var/www/heltour.lakin.ca/env/bin/gunicorn -w 4 -b 127.0.0.1:8580  heltour.wsgi:application
+/var/www/heltour.lakin.ca/env/bin/gunicorn --capture-output --error-logfile /var/log/heltour/error.log -w 4 -b 127.0.0.1:8580  heltour.wsgi:application
 
