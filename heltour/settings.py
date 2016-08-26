8eec64d3a79ab66998d6b28ce0c78319d8d4a0a1
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-08-25 22:04:15 -0400

    Modify the footer message for the staging env

--- heltour/settings.py
+++ heltour/settings.py
@@ -2,7 +2,9 @@ import os
 
 if 'HELTOUR_ENV' in os.environ and os.environ['HELTOUR_ENV'] == 'STAGING':
     from staging_settings import *
+    STAGING = True
 elif 'HELTOUR_ENV' in os.environ and os.environ['HELTOUR_ENV'] == 'LIVE':
     from live_settings import *
+    STAGING = True
 else:
     raise Exception('HELTOUR_ENV environment variable not set (should be set in wsgi.py)')
