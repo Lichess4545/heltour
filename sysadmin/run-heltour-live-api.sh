c6fd3882539d47042c783ec1cd0b15b2f5d962d6
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-09-02 13:08:21 -0400

    Reduce the number of gunicorn workers for the API worker

--- sysadmin/run-heltour-live-api.sh
+++ sysadmin/run-heltour-live-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/www.lichess4545.com/
 export PYTHONPATH=/var/www/www.lichess4545.com/
-/var/www/www.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /var/log/heltour/error-api.log -t 60 -w 4 -b 127.0.0.1:8880  heltour.live_api_wsgi:application
+/var/www/www.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /var/log/heltour/error-api.log -t 60 -w 2 -b 127.0.0.1:8880  heltour.live_api_wsgi:application
 
