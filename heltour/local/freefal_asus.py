c708582c943af6317144555133f092b0708c5965
Mark Henle <freefal67@gmail.com>
2016-09-01 23:32:52 -0400

    On TV page, replace rating with team name for team leagues

---
+++ heltour/local/freefal_asus.py
@@ -0,0 +1,8 @@
+DEBUG = True
+GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/home/freefal/Downloads/client_id.json'
+SLACK_API_TOKEN_FILE_PATH = '/home/ben/slack-token'
+JAVAFO_COMMAND = 'java -jar /home/ben/javafo.jar'
+
+INTERNAL_IPS = ['127.0.0.1', '192.168.56.100']
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
