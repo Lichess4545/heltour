585d449c434de29645c886205aa61242aa12ab8d
Lakin Wecker <lakin@structuredabstraction.com>
2016-09-01 21:19:05 -0600

    Initial, probably buggy version of the api worker stuff

---
+++ heltour/live_api_wsgi.py
@@ -0,0 +1,26 @@
+#!/var/www/staging.lichess4545.com/env/bin/python
+"""
+WSGI config for heltour project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+import os
+
+local_dir = os.path.join(os.path.dirname(__file__))
+activate_this = '/var/www/staging.lichess4545.com/env/bin/activate_this.py'
+if os.path.exists(activate_this):
+    execfile(activate_this, dict(__file__=activate_this))
+
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("HELTOUR_APP", "API_WORKER")
+os.environ.setdefault("HELTOUR_ENV", "LIVE")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heltour.settings")
+
+application = get_wsgi_application()
+
+
