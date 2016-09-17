80f99f1f48dcd85ff9560a4e2786a80f2fe3c85f
Lakin Wecker <lakin@structuredabstraction.com>
2016-09-17 10:15:33 -0600

    pointing to my slack token

--- heltour/local/blackout.py
+++ heltour/local/blackout.py
@@ -1,4 +1,6 @@
 DEBUG = True
 GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/media/Crypt/Work/Development/lakin.heltour/gspread.conf'
+SLACK_API_TOKEN_FILE_PATH = '/home/lakin/Work/Development/lakin.heltour/slack-token.conf'
 
 INTERNAL_IPS = ['127.0.0.1', '192.168.56.100']
+
