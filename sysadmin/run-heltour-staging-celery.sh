8e40dbc426c772b0d163d093e5684a4df899ed42
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-09-19 18:46:09 -0400

    Fix typo in celery sh file

--- sysadmin/run-heltour-staging-celery.sh
+++ sysadmin/run-heltour-staging-celery.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export HELTOUR_ENV=STAGING
-cd /var/www/staging.liches4545.com
+cd /var/www/staging.lichess4545.com
 /var/www/staging.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/staging.heltour/celery.log
-
