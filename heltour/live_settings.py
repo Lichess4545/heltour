b672eb285d4b4a612694186705a9602023f4760c
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-09-19 18:46:21 -0400

    Enable TV task on live

--- heltour/live_settings.py
+++ heltour/live_settings.py
@@ -169,11 +169,11 @@ CELERYBEAT_SCHEDULE = {
         'schedule': timedelta(minutes=30),
         'args': ()
     },
-#     'update-tv-state': {
-#         'task': 'heltour.tournament.tasks.update_tv_state',
-#         'schedule': timedelta(minutes=5),
-#         'args': ()
-#     },
+    'update-tv-state': {
+        'task': 'heltour.tournament.tasks.update_tv_state',
+        'schedule': timedelta(minutes=5),
+        'args': ()
+    },
     'update-slack-users': {
         'task': 'heltour.tournament.tasks.update_slack_users',
         'schedule': timedelta(minutes=30),
