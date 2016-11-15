5f87c606e77ef9b04e9035054be61cf0d4fc8759
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-11-14 19:54:04 -0500

    Increase concurrency for celery workers

--- sysadmin/run-heltour-live-celery.sh
+++ sysadmin/run-heltour-live-celery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export HELTOUR_ENV=LIVE
 cd /var/www/www.lichess4545.com/
-/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log 
+/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log -c 4
 
