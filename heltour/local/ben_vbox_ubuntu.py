5db4e9067a28b313dd4b6f9df2a29141387b3fe9
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-12-19 22:24:51 -0500

    Update dev config

--- heltour/local/ben_vbox_ubuntu.py
+++ heltour/local/ben_vbox_ubuntu.py
@@ -2,6 +2,8 @@ DEBUG = True
 GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/home/ben/gspread-creds.json'
 SLACK_API_TOKEN_FILE_PATH = '/home/ben/slack-token'
 SLACK_WEBHOOK_FILE_PATH = '/home/ben/slack-webhook'
+LICHESS_CREDS_FILE_PATH = '/home/ben/lichess-creds'
+LICHESS_DOMAIN = 'https://en.stage.lichess.org/'
 JAVAFO_COMMAND = 'java -jar /home/ben/javafo.jar'
 
 INTERNAL_IPS = ['127.0.0.1', '192.168.56.100']
