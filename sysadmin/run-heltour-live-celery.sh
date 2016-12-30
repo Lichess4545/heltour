6e13017bf3955569414c0da8c4f67a9e85a67cc0
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-12-30 13:00:01 -0500

    Add -Ofair argument to celery to prevent unnecessary contention

--- sysadmin/run-heltour-live-celery.sh
+++ sysadmin/run-heltour-live-celery.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export HELTOUR_ENV=LIVE
 cd /var/www/www.lichess4545.com/
-/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log -c 4 --loglevel=DEBUG
-
+/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log -c 4 --loglevel=DEBUG -Ofair
