d1e72e48a54c974c997635f6c6f04f73f20e8637
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-12-31 10:58:51 -0500

    Implement logic to determine the current alternate search round

--- heltour/tournament/alternates_manager.py
+++ heltour/tournament/alternates_manager.py
@@ -5,14 +5,28 @@ import reversion
 def current_round(season):
     if not season.alternates_manager_enabled():
         return None
-    return season.round_set.filter(publish_pairings=True, is_completed=False).order_by('number').first()
+    # Figure out which round we should be running a search for
+    # Depends on the round state, current date and alternates manager settings
+    last_published = season.round_set.filter(publish_pairings=True).order_by('-number').first()
+    if last_published is None:
+        return None
+    setting = season.alternates_manager_setting()
+    if not setting.contact_before_round_start:
+        current = last_published
+    elif timezone.now() < last_published.end_date - setting.contact_offset_before_round_start:
+        current = last_published
+    else:
+        current = season.round_set.filter(number=last_published.number + 1).first() or last_published
+    if current.is_completed:
+        return None
+    return current
 
 def do_alternate_search(season, board_number):
     round_ = current_round(season)
     if round_ is None:
         return
     setting = season.alternates_manager_setting()
-    print 'Alternate search on bd %d' % board_number
+    print 'Alternate search on bd %d for round %d' % (board_number, round_.number)
 
     # Figure out which players need to be replaced and which alternates have/haven't been contacted
     player_availabilities = PlayerAvailability.objects.filter(round=round_, is_available=False) \
