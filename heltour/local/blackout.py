b76d26f7cea28cca505863464740d8bfcb7cd750
Lakin Wecker <lakin@lakin.ca>
2017-01-07 21:08:45 -0700

    Adding the location of my javafo to my local settings.

--- heltour/local/blackout.py
+++ heltour/local/blackout.py
@@ -3,4 +3,5 @@ GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/media/Crypt/Work/Development/lakin.helto
 SLACK_API_TOKEN_FILE_PATH = '/home/lakin/Work/Development/lakin.heltour/slack-token.conf'
 
 INTERNAL_IPS = ['127.0.0.1', '192.168.56.100']
+JAVAFO_COMMAND = 'java -jar /home/lakin/Personal-Repos/heltour/javafo.jar'
 
