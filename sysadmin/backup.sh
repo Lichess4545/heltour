3a8d3b89a61f6d832e8a41c62d0948d150756203
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-01-21 13:53:19 -0500

    Update backup script port

--- sysadmin/backup.sh
+++ sysadmin/backup.sh
@@ -5,7 +5,7 @@
 # 15 * * * * /var/www/www.lichess4545.com/current/sysadmin/backup.sh
 OF=/var/backups/heltour-sql/hourly/heltour-`date +%Y-%m-%d-%H%M`.sql
 BZOF=/var/backups/heltour-sql/hourly/heltour-`date +%Y-%m-%d-%H%M`.sql.bz2
-pg_dump --clean --no-owner --no-privileges --format=plain --host localhost heltour_lichess4545 --username heltour_lichess4545 >& $OF
+pg_dump --clean --no-owner --no-privileges --format=plain --host localhost heltour_lichess4545 --username heltour_lichess4545 --port 5433 >& $OF
 bzip2 $OF
 ln -sf $BZOF /var/backups/heltour-sql/hourly/latest.sql.bz2
 /var/www/www.lichess4545.com/env/bin/python /var/www/www.lichess4545.com/current/sysadmin/backup.py
