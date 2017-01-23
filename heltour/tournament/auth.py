4cbd3c7f93ad4b31eda2948e17c8f1f10a10d163
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-01-22 21:45:03 -0500

    Fix issue preventing deletion

--- heltour/tournament/auth.py
+++ heltour/tournament/auth.py
@@ -4,6 +4,11 @@ class LeagueAuthBackend(object):
     def has_perm(self, user_obj, perm, obj=None):
         if isinstance(obj, League):
             return LeagueModerator.objects.filter(league=obj, player__lichess_username__iexact=user_obj.username).exists()
+        if obj is None and perm.startswith('tournament.delete_'):
+            # Have to work around a django bug preventing deletion
+            # https://code.djangoproject.com/ticket/13539
+            # There should be an object-specific check that will be sufficient to prevent unauthorized deletion
+            return LeagueModerator.objects.filter(player__lichess_username__iexact=user_obj.username).exists()
         return False
 
     def has_module_perms(self, user_obj, app_label):
