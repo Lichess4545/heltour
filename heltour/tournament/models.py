57c594a600f7e746f2721a361336ef593bb9592c
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-03-20 14:11:41 -0400

    Order seasons by league/name in the admin

--- heltour/tournament/models.py
+++ heltour/tournament/models.py
@@ -155,6 +155,7 @@ class Season(_BaseModel):
             ('manage_players', 'Can manage players'),
             ('review_nominated_games', 'Can review nominated games'),
         )
+        ordering = ['league__name', '-name']
 
     def __init__(self, *args, **kwargs):
         super(Season, self).__init__(*args, **kwargs)
