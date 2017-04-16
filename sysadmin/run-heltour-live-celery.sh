e069f25d3265abf620a6cc00c11f3e3e84090f99
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-04-16 10:53:46 -0400

    Trim the number of processes running on the server

--- sysadmin/run-heltour-live-celery.sh
+++ sysadmin/run-heltour-live-celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export HELTOUR_ENV=LIVE
 cd /var/www/www.lichess4545.com/
-/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log -c 4 --loglevel=DEBUG -Ofair
+/var/www/www.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/heltour/celery.log -c 3 --loglevel=DEBUG -Ofair
--- sysadmin/run-heltour-staging-celery.sh
+++ sysadmin/run-heltour-staging-celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export HELTOUR_ENV=STAGING
 cd /var/www/staging.lichess4545.com
-/var/www/staging.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/staging.heltour/celery.log -c 2 -Ofair
+/var/www/staging.lichess4545.com/env/bin/celery -A heltour worker -B -f /var/log/staging.heltour/celery.log -c 1 -Ofair
