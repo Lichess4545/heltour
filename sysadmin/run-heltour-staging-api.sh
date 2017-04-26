d16fd0a90bdd5f84d976e948f1e0f8d62fa56b3d
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-04-25 23:25:18 -0400

    Increase number of processes back to optimal levels

--- sysadmin/run-heltour-staging-api.sh
+++ sysadmin/run-heltour-staging-api.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/staging.lichess4545.com/
 export PYTHONPATH=/var/www/staging.lichess4545.com/
-/var/www/staging.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /var/log/staging.heltour/error-api.log -t 60 -w 1 -b 127.0.0.1:8780  heltour.staging_api_wsgi:application
+/var/www/staging.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /var/log/staging.heltour/error-api.log -t 60 -w 2 -b 127.0.0.1:8780  heltour.staging_api_wsgi:application
 
