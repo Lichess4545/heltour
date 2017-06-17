0109ed87a701d7cfc806dd0f900791f214e727d0
Lakin Wecker <lakin@structuredabstraction.com>
2017-06-17 13:29:47 -0600

    appropriate log file

--- sysadmin/run-heltour-live.sh
+++ sysadmin/run-heltour-live.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/lichess4545/web/www.lichess4545.com/
 export PYTHONPATH=/home/lichess4545/web/www.lichess4545.com/
-/home/lichess4545/web/www.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /home/lichess4545/web/heltour/error.log -t 300 -w 4 -b 127.0.0.1:8580  heltour.wsgi:application
+/home/lichess4545/web/www.lichess4545.com/env/bin/gunicorn --capture-output --error-logfile /home/lichess4545/log/heltour/error.log -t 300 -w 4 -b 127.0.0.1:8580  heltour.wsgi:application
 
