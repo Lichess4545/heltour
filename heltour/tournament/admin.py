0b92ba887aae41deb05617523045f6329f23ee1b
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-09-24 21:05:29 -0400

    Fix slack filter

--- heltour/tournament/admin.py
+++ heltour/tournament/admin.py
@@ -1757,9 +1757,10 @@ class InSlackFilter(SimpleListFilter):
         )
 
     def queryset(self, request, queryset):
-        if self.value() in ('0', '1'):
-            kwargs = { '{0}__isnull'.format(self.parameter_name) : self.value() == '1' }
-            return queryset.filter(**kwargs)
+        if self.value() == '0':
+            return queryset.filter(player__slack_user_id='')
+        if self.value() == '1':
+            return queryset.exclude(player__slack_user_id='')
         return queryset
 
 #-------------------------------------------------------------------------------
