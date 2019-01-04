9280120afc23388f7feb5a1cf934c977b1e40dd4
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-01-04 14:07:40 -0500

    Specify python 3.6

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-virtualenv env --no-site-packages --prompt="(heltour):" --python=/usr/bin/python3
+virtualenv env --no-site-packages --prompt="(heltour):" --python=/usr/bin/python3.6
 source env/bin/activate
 pip install -r requirements.txt
 fab update
