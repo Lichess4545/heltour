2d24b1b2f9fd5c661a48f233480ab09322d6bf91
Martin Colwell <colwem@gmail.com>
2019-01-29 14:26:05 -0500

    Add config to local config to enable login to work

--- heltour/local/martinsmacmini.py
+++ heltour/local/martinsmacmini.py
@@ -3,6 +3,10 @@ DEBUG = True
 # SLACK_API_TOKEN_FILE_PATH = '/home/vagrant/heltour/slack-token.conf'
 
 INTERNAL_IPS = ['127.0.0.1']
+
+LICHESS_CREDS_FILE_PATH = '/Users/colwem/workshop/heltour/.ignore/lichess.conf'
+LINK_PROTOCOL = 'http'
+SITE_ID = 2
 # JAVAFO_COMMAND = 'java -jar /home/vagrant/heltour/javafo.jar'
 # STATIC_ROOT = '/home/vagrant/heltour/static'
 
