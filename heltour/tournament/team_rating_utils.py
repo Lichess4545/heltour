f6e2145d3a70a25628906a67843855c7b28a4110
Martin Colwell <colwem@gmail.com>
2019-03-19 19:27:11 -0400

    Fix error caused when a team has no members and hence an average rating of NONE

--- heltour/tournament/team_rating_utils.py
+++ heltour/tournament/team_rating_utils.py
@@ -13,12 +13,14 @@ def variance(mean, xs):
 #-------------------------------------------------------------------------------
 def team_rating_variance(teams, expected_rating=False):
     means = [team.get_mean(expected_rating) for team in teams]
+    means = [mean for mean in means if mean is not None]
     league_mean = sum(means) / len(teams)
     return variance(league_mean, means)
 
 #-------------------------------------------------------------------------------
 def team_rating_range(teams, expected_rating=False):
     means = [team.get_mean(expected_rating) for team in teams]
+    means = [mean for mean in means if mean is not None]
     return max(means) - min(means)
 
 
