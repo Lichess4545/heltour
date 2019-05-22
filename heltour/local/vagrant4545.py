9255f1c324fb73d9618552755b4f77a9a3d6f67a
Martin Colwell <colwem@gmail.com>
2019-05-22 00:13:50 -0400

    Update config for vagrant

--- heltour/local/vagrant4545.py
+++ heltour/local/vagrant4545.py
@@ -1,7 +1,7 @@
 DEBUG = True
-GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/home/vagrant/heltour/gspread.conf'
-SLACK_API_TOKEN_FILE_PATH = '/home/vagrant/heltour/slack-token.conf'
-SLACK_WEBHOOK_FILE_PATH ='/home/vagrant/heltour/slack-webhook.conf'
+GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/home/vagrant/heltour/.ignore/gspread.conf'
+SLACK_API_TOKEN_FILE_PATH = '/home/vagrant/heltour/.ignore/slack-token.conf'
+SLACK_WEBHOOK_FILE_PATH ='/home/vagrant/heltour/.ignore/slack-webhook.conf'
 LICHESS_CREDS_FILE_PATH = '/home/vagrant/heltour/.ignore/lichess.conf'
 LINK_PROTOCOL = 'http'
 SITE_ID = 1
@@ -12,5 +12,31 @@ STATIC_ROOT = '/home/vagrant/heltour/static'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-BOT_USER_ID = 'UGHM82CG2'
+SLACK_HOST = 'zhteamtesting.slack.com'
+BOT_USER_ID = 'UGSUFP605'
 BOT_NAME = 'crazybot'
+TASK_SERIALIZER = 'json'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+    # root logger
+        '': {
+            'level': 'INFO',
+            'handlers': ['console'],
+        },
+    },
+}
