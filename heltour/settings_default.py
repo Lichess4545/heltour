ea1d97ecaf7a8946ed74c6010147358d3c4acd9a
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-12-16 23:46:56 -0500

    Fix STATIC_ROOT relative path

--- heltour/settings_default.py
+++ heltour/settings_default.py
@@ -228,8 +228,7 @@ CACHES = {
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
-STATIC_PRECOMPILER_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
