061db2b305082cd40e1699fad02b28c39cf70107
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-12-16 22:32:06 -0500

    Fix settings file pointer

--- manage_staging.py
+++ manage_staging.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("HELTOUR_ENV", "STAGING")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heltour.staging_settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "heltour.settings")
 
     from django.core.management import execute_from_command_line
 
