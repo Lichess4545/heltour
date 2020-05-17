a678bea94a479610e6ebb7539d81d47422b8d2cb
Lakin Wecker <lakin@structuredabstraction.com>
2020-05-17 08:04:24 -0600

    Fixing my local setup

--- heltour/local/sebbers.py
+++ heltour/local/sebbers.py
@@ -1,6 +1,7 @@
 DEBUG = True
 GOOGLE_SERVICE_ACCOUNT_KEYFILE_PATH = '/home/lakin/work/development/heltour/gspread.conf'
 SLACK_API_TOKEN_FILE_PATH = '/home/lakin/work/development/heltour/slack-token.conf'
+SLACK_WEBHOOK_FILE_PATH = '/home/lakin/personal-repos/lichess/heltour/webhook.txt'
 
 INTERNAL_IPS = ['127.0.0.1', '192.168.1.19']
-JAVAFO_COMMAND = 'java -jar /home/lakin/personal-repos/heltour/javafo.jar'
+JAVAFO_COMMAND = 'java -jar /home/lakin/personal-repos/lichess/heltour/javafo.jar'
