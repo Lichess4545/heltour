f802987f3fb408ca46603f653a35d8c167116044
Lakin Wecker <lakin@structuredabstraction.com>
2022-02-05 13:36:50 -0700

    Fixing the update-requirements.sh files

--- sysadmin/update-requirements-live.sh
+++ sysadmin/update-requirements-live.sh
@@ -2,6 +2,6 @@
 pushd /home/lichess4545/web/www.lichess4545.com/
 source /home/lichess4545/web/www.lichess4545.com/env/bin/activate
 pushd /home/lichess4545/web/www.lichess4545.com/current/
-/home/lichess4545/web/www.lichess4545.com/env/bin/pip install -r /home/lichess4545/web/www.lichess4545.com/current/sysadmin/heltour-requirements.txt
+/home/lichess4545/web/www.lichess4545.com/env/bin/poetry install
 popd
 popd
