e6eace5e8f45282bf2adbb1e62516f5e29ea23a0
glbert <tachydromos@hotmail.com>
2023-03-10 17:31:04 +0100

    rejection mail

--- heltour/tournament/admin.py
+++ heltour/tournament/admin.py
@@ -2115,9 +2115,26 @@ class RegistrationAdmin(_BaseAdmin):
                         reg.status_changed_date = timezone.now()
                         reg.save()
 
+
                     self.message_user(request,
                                       'Registration for "%s" rejected.' % reg.lichess_username,
                                       messages.INFO)
+                    try:
+                        send_mail(
+                            render_to_string('tournament/emails/registration_rejected_subject.txt',
+                                             {'reg': reg}),
+                            render_to_string('tournament/emails/registration_rejected.txt',
+                                             {'reg': reg}),
+                            settings.DEFAULT_FROM_EMAIL,
+                            [reg.email],
+                            html_message=render_to_string('tournament/emails/registration_rejected.html',
+                                                          {'reg': reg})
+                        )
+                        self.message_user(request, 'Rejection email sent to "%s".' % reg.email,
+                                          messages.INFO)
+                    except SMTPException:
+                        logger.exception('A rejection email could not be sent.')
+
                     return redirect_with_params('admin:tournament_registration_changelist',
                                                 params='?' + changelist_filters)
                 else:
