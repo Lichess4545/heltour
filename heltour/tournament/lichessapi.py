fbdcba7dde99016aae85ed80eb3b2abf88942d52
glbert <tachydromos@hotmail.com>
2024-12-21 17:32:53 +0100

    fix missing content => content_type

--- heltour/tournament/lichessapi.py
+++ heltour/tournament/lichessapi.py
@@ -170,8 +170,8 @@ def get_peak_rating(lichess_username, perf_type):
 
 
 def bulk_start_games(tokens, clock, increment, clockstart, variant, leaguename, priority=0, max_retries=0, timeout=30):
-    url = f'{settings.API_WORKER_HOST}/lichessapi/api/bulk-pairing?priority={priority}&max_retries={max_retries}&content=application/x-www-form-urlencoded'
-    post = f'players={tokens}&clock.limit={clock}&clock.increment={increment}&startClocksAt={clockstart}&rated=true&variant={variant}&message=Hello! Your {leaguename} game with {{opponent}} is ready. Please join it at {{game}}. Clocks will be started in 6 minutes, but you can begin playing at any time.&rules=noClaimWin'
+    url = f'{settings.API_WORKER_HOST}/lichessapi/api/bulk-pairing?priority={priority}&max_retries={max_retries}&content_type=application/x-www-form-urlencoded'
+    post = f'players={tokens}&clock.limit={clock}&clock.increment={increment}&startClocksAt={clockstart}&rated=true&variant={variant}&message=Hello! Your {leaguename} game with {{opponent}} is ready. Please join it at {{game}}%0AClocks will be started in 6 minutes, but you can begin playing at any time.&rules=noClaimWin'
     result = _apicall_with_error_parsing(url=url, timeout=timeout, post_data=post)
     return json.loads(result)
     
