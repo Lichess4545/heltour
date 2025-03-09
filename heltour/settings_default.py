850f502c1a7cbf39ae9c7ff68b517533b738cb3d
Lakin Wecker <lakin@wecker.ca>
2025-03-09 14:03:42 -0600

    fix: Add this setting

--- heltour/settings_default.py
+++ heltour/settings_default.py
@@ -38,6 +38,13 @@ ALLOWED_HOSTS = [
     'heltour.lakin.ca',
     'localhost',
 ]
+CSRF_TRUSTED_ORIGINS = [
+    'www.lichess4545.tv',
+    'lichess4545.tv',
+    'www.lichess4545.com',
+    'lichess4545.com',
+    'localhost',
+]
 LINK_PROTOCOL = 'https'
 
 # Application definition
