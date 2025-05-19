884bc554636ff30d81f68ce0b85b309376140af7
glbert-does <tachydromos@hotmail.com>
2025-05-19 16:27:51 +0200

    fix automod tests, add new ones for full automod_noshow coverage

--- heltour/tournament/tests/test_automod.py
+++ heltour/tournament/tests/test_automod.py
@@ -19,40 +19,85 @@ class NoShowTestCase(TestCase):
 
     @patch('heltour.tournament.signals.notify_noshow.send')
     def test_has_moves(self, noshow_sender):
-        LonePlayerPairing.objects.filter(round=self.rd, white=self.player1, black=self.player2).update(tv_state='has_moves')
+        self.pairing.tv_state = 'has_moves'
         automod_noshow(self.pairing)
         # pairing has moves, so the noshow_sender should not have been called.
         self.assertFalse(noshow_sender.called)
 
     @patch('heltour.tournament.signals.notify_noshow.send')
     def test_has_result(self, noshow_sender):
-        LonePlayerPairing.objects.filter(round=self.rd, white=self.player1, black=self.player2).update(result='1-0')
+        self.pairing.result = '1-0'
         automod_noshow(self.pairing)
         # pairing has a result, so the noshow_sender should not have been called.
         self.assertFalse(noshow_sender.called)
 
     @patch('heltour.tournament.signals.notify_noshow.send')
     def test_has_game_link(self, noshow_sender):
-        LonePlayerPairing.objects.filter(round=self.rd, white=self.player1, black=self.player2).update(game_link="https://lichess.org/NKop9IyD")
+        self.pairing.game_link = 'https://lichess.org/NKop9IyD'
         automod_noshow(self.pairing)
         # pairing has a game_link, so the noshow_sender should not have been called.
         self.assertFalse(noshow_sender.called)
 
     @patch('heltour.tournament.signals.notify_noshow.send')
-    def test_nosetting_confirmed_black_noshows(self, noshow_sender):
-        # pairing2 has no moves, and one of the players is present while the other is not, so noshow_sender should have been called.
+    def test_no_confirmed_black_noshows(self, noshow_sender):
         PlayerPresence.objects.filter(player=self.player1, pairing=self.pairing, round=self.rd).update(online_for_game=False)
         PlayerPresence.objects.filter(player=self.player2, pairing=self.pairing, round=self.rd).update(online_for_game=True)
         automod_noshow(self.pairing)
         self.assertTrue(noshow_sender.called)
-        self.assertEqual(noshow_sender.call_args[1]["player"], self.pairing.black)
-        self.assertEqual(noshow_sender.call_args[1]["opponent"], self.pairing.white)
+        # assert noshow by black
+        self.assertEqual(noshow_sender.call_args[1]['player'], self.pairing.black)
+        self.assertEqual(noshow_sender.call_args[1]['opponent'], self.pairing.white)
 
     @patch('heltour.tournament.signals.notify_noshow.send')
-    def test_nosetting_confirmed_white_noshows(self, noshow_sender):
-        PlayerPresence.objects.filter(player=self.player2, pairing=self.pairing, round=self.rd).update(online_for_game=False)
+    def test_no_confirmed_white_noshows(self, noshow_sender):
         PlayerPresence.objects.filter(player=self.player1, pairing=self.pairing, round=self.rd).update(online_for_game=True)
+        PlayerPresence.objects.filter(player=self.player2, pairing=self.pairing, round=self.rd).update(online_for_game=False)
         automod_noshow(self.pairing)
         self.assertTrue(noshow_sender.called)
-        self.assertEqual(noshow_sender.call_args[1]["player"], self.pairing.white)
-        self.assertEqual(noshow_sender.call_args[1]["opponent"], self.pairing.black)
+        # assert noshow by black
+        self.assertEqual(noshow_sender.call_args[1]['player'], self.pairing.white)
+        self.assertEqual(noshow_sender.call_args[1]['opponent'], self.pairing.black)
+
+    @patch('heltour.tournament.lichessapi.get_game_meta',
+            return_value={'moves': '1.e4 e5 2.Ke2'})
+    @patch('heltour.tournament.signals.notify_noshow.send')
+    def test_confirmed_has_moves(self, noshow_sender, game_meta):
+        self.pairing.white_confirmed=True
+        self.pairing.black_confirmed=True
+        self.pairing.game_link='https://lichess.org/NKop9IyD'
+        automod_noshow(self.pairing)
+        self.assertTrue(game_meta.called)
+        # game_meta indicates that there are moves, there is no noshow.
+        self.assertFalse(noshow_sender.called)
+
+    @patch('heltour.tournament.lichessapi.get_game_meta',
+            return_value={'moves': '1.e4'})
+    @patch('heltour.tournament.signals.notify_noshow.send')
+    def test_confirmed_black_no_shows(self, noshow_sender, game_meta):
+        PlayerPresence.objects.filter(player=self.player1, pairing=self.pairing, round=self.rd).update(online_for_game=True)
+        PlayerPresence.objects.filter(player=self.player2, pairing=self.pairing, round=self.rd).update(online_for_game=False)
+        self.pairing.white_confirmed=True
+        self.pairing.black_confirmed=True
+        self.pairing.game_link='https://lichess.org/NKop9IyD'
+        automod_noshow(self.pairing)
+        self.assertTrue(game_meta.called)
+        self.assertTrue(noshow_sender.called)
+        # assert noshow by black
+        self.assertEqual(noshow_sender.call_args[1]['player'], self.pairing.white)
+        self.assertEqual(noshow_sender.call_args[1]['opponent'], self.pairing.black)
+
+    @patch('heltour.tournament.lichessapi.get_game_meta',
+            return_value={'moves': '1.e4'})
+    @patch('heltour.tournament.signals.notify_noshow.send')
+    def test_confirmed_white_no_shows(self, noshow_sender, game_meta):
+        PlayerPresence.objects.filter(player=self.player1, pairing=self.pairing, round=self.rd).update(online_for_game=False)
+        PlayerPresence.objects.filter(player=self.player2, pairing=self.pairing, round=self.rd).update(online_for_game=True)
+        self.pairing.white_confirmed=True
+        self.pairing.black_confirmed=True
+        self.pairing.game_link='https://lichess.org/NKop9IyD'
+        automod_noshow(self.pairing)
+        self.assertTrue(game_meta.called)
+        # assert a noshow by white
+        self.assertTrue(noshow_sender.called)
+        self.assertEqual(noshow_sender.call_args[1]['player'], self.pairing.black)
+        self.assertEqual(noshow_sender.call_args[1]['opponent'], self.pairing.white)
