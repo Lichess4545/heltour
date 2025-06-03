715d2d1069e5ad4e6df24466c744a093dc96063e
glbert-does <tachydromos@hotmail.com>
2025-06-03 15:40:37 +0200

    add hasher, potentially use different database for tests

--- heltour/settings_testing.py
+++ heltour/settings_testing.py
@@ -9,3 +9,16 @@ STORAGES = {
             'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage'
         },
 }
+
+# use a faster hasher for tests
+PASSWORD_HASHERS = [
+        "django.contrib.auth.hashers.MD5PasswordHasher"
+]
+
+# using sqlite3 in memory speeds up tests
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': ':memory:',
+#    }
+#}
