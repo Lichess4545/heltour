035b68c7472fbc4224cd9db07fd8cc02e5b0b1e2
glber-does <tachydromos@hotmail.com>
2025-06-07 16:00:57 +0200

    add command for migrations

--- heltour/commands.py
+++ heltour/commands.py
@@ -1,5 +1,6 @@
 from subprocess import call
 from os import environ
+from sys import argv
 
 def runserver():
     call(["python", "manage.py", "runserver", "0.0.0.0:8000"])
@@ -8,7 +9,19 @@ def runapiworker():
     call(["python", "manage.py", "runserver", "0.0.0.0:8880"], env=dict(environ, HELTOUR_APP="API_WORKER"))
 
 def test():
-    call(["python", "-Wall", "manage.py", "test"])
+    if len(argv) == 1:
+        call(["python", "-Wall", "manage.py", "test"])
+    else:
+        call(["python", "-Wall", "manage.py", "test"] + argv[1:])
 
-def coverage():
-    call(["coverage", "run", "--branch", "manage.py", "test"])
+def showmigrations():
+    if len(argv) == 1:
+        call(["python", "manage.py", "showmigrations"])
+    else:
+        call(["python", "manage.py", "showmigrations"] + argv[1:])
+
+def migrate():
+    if len(argv) == 1:
+        call(["python", "manage.py", "migrate"])
+    else:
+        call(["python", "manage.py", "migrate"] + argv[1:])
