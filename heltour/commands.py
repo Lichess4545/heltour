e10bea854986b24f2a4546a65dcf0044c83e2236
glber-does <tachydromos@hotmail.com>
2025-06-08 09:56:44 +0200

    add coverage

--- heltour/commands.py
+++ heltour/commands.py
@@ -2,11 +2,14 @@ from subprocess import call
 from os import environ
 from sys import argv
 
+
 def runserver():
     call(["python", "manage.py", "runserver", "0.0.0.0:8000"])
 
+
 def runapiworker():
     call(["python", "manage.py", "runserver", "0.0.0.0:8880"], env=dict(environ, HELTOUR_APP="API_WORKER"))
+
 
 def test():
     if len(argv) == 1:
@@ -14,11 +17,18 @@ def test():
     else:
         call(["python", "-Wall", "manage.py", "test"] + argv[1:])
 
+
+def testcov():
+    call(["coverage", "run", "manage.py", "test"])
+    call(["coverage", "html"])
+
+
 def showmigrations():
     if len(argv) == 1:
         call(["python", "manage.py", "showmigrations"])
     else:
         call(["python", "manage.py", "showmigrations"] + argv[1:])
+
 
 def migrate():
     if len(argv) == 1:
