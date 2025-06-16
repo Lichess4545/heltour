39f4837e30b95e22839e9c973b4aa07e8770c14b
glbert-does <tachydromos@hotmail.com>
2025-06-16 15:21:35 +0200

    lint test_workflows.py

--- heltour/tournament/tests/test_workflows.py
+++ heltour/tournament/tests/test_workflows.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 from heltour.tournament.models import AlternatesManagerSetting, Player, TeamMember
-from heltour.tournament.workflows import ApproveRegistrationWorkflow, UpdateBoardOrderWorkflow
-from heltour.tournament.tests.testutils import createCommonLeagueData, create_reg, get_season, Shush
+from heltour.tournament.workflows import (
+    ApproveRegistrationWorkflow,
+    UpdateBoardOrderWorkflow,
+)
+from heltour.tournament.tests.testutils import (
+    createCommonLeagueData,
+    create_reg,
+    get_season,
+    Shush,
+)
 
 
 class TestLJPCase(TestCase):
@@ -10,7 +18,12 @@ class TestLJPCase(TestCase):
         createCommonLeagueData(round_count=7)
 
     def test_ljp_none_rating(self, *args):
-        Player.objects.create(lichess_username="newplayer", profile={"perfs": {"bullet": {"games": 50, "rating": 1650, "rd": 45, "prog": 10}}})
+        Player.objects.create(
+            lichess_username="newplayer",
+            profile={
+                "perfs": {"bullet": {"games": 50, "rating": 1650, "rd": 45, "prog": 10}}
+            },
+        )
         season = get_season("lone")
         # creating a reg writes to the log, disable that temporarily for nicer test output
         with Shush():
@@ -25,18 +38,22 @@ class UpdateBoardOrderWorkflowTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         createCommonLeagueData()
-        cls.s = get_season('team')
+        cls.s = get_season("team")
         AlternatesManagerSetting(league=cls.s.league)
         cls.ubo = UpdateBoardOrderWorkflow(cls.s)
         cls.players = []
         rating = 1000
         for player in Player.objects.all():
             rating += 100
-            Player.objects.filter(pk=player.pk).update(profile={"perfs": {"classical": {"rating": rating}}})
+            Player.objects.filter(pk=player.pk).update(
+                profile={"perfs": {"classical": {"rating": rating}}}
+            )
             cls.players.append(player)
 
     def test_lonewolf(self):
-        self.assertEqual(UpdateBoardOrderWorkflow(get_season('lone')).run(alternates_only=True), None)
+        self.assertEqual(
+            UpdateBoardOrderWorkflow(get_season("lone")).run(alternates_only=True), None
+        )
 
     def test_team_board_order(self):
         self.assertEqual(TeamMember.objects.get(player=self.players[0]).board_number, 1)
