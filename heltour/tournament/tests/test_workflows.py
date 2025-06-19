5217d997544e7c29c5f4146fcf24c44a22fc0933
glbert-does <tachydromos@hotmail.com>
2025-06-19 18:10:58 +0200

    add test for approve_reg in ApproveRegistrationWorkflow

--- heltour/tournament/tests/test_workflows.py
+++ heltour/tournament/tests/test_workflows.py
@@ -1,37 +1,75 @@
+from unittest.mock import PropertyMock, patch
+
+from django.contrib.auth.models import User
 from django.test import TestCase
-from heltour.tournament.models import AlternatesManagerSetting, Player, TeamMember
+from django.test.client import RequestFactory
+
+from heltour.tournament.models import (
+    AlternatesManagerSetting,
+    Player,
+    SeasonPlayer,
+    TeamMember,
+)
+from heltour.tournament.tests.testutils import (
+    Shush,
+    create_reg,
+    createCommonLeagueData,
+    get_season,
+)
 from heltour.tournament.workflows import (
     ApproveRegistrationWorkflow,
     UpdateBoardOrderWorkflow,
 )
-from heltour.tournament.tests.testutils import (
-    createCommonLeagueData,
-    create_reg,
-    get_season,
-    Shush,
-)
 
 
-class TestLJPCase(TestCase):
+class ApproveRegistrationWorkflowTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
         createCommonLeagueData(round_count=7)
-
-    def test_ljp_none_rating(self, *args):
         Player.objects.create(
             lichess_username="newplayer",
             profile={
                 "perfs": {"bullet": {"games": 50, "rating": 1650, "rd": 45, "prog": 10}}
             },
+            rating=1650,
         )
-        season = get_season("lone")
-        # creating a reg writes to the log, disable that temporarily for nicer test output
+        cls.superuser = User.objects.create(
+            username="superuser", password="password", is_superuser=True, is_staff=True
+        )
+        cls.season = get_season("lone")
+        # creating a reg writes to the log,
+        # disable that temporarily for nicer test output
         with Shush():
-            reg = create_reg(season, "newplayer")
-        arw = ApproveRegistrationWorkflow(reg, 4)
-        self.assertEqual(arw.default_byes, 2)
-        self.assertEqual(arw.active_round_count, 3)
-        self.assertEqual(arw.default_ljp, 0)
+            cls.reg = create_reg(cls.season, "newplayer")
+        cls.arw = ApproveRegistrationWorkflow(cls.reg, 4)
+        cls.rf = RequestFactory()
+
+    def test_ljp_none_rating(self):
+        self.assertEqual(self.arw.default_byes, 2)
+        self.assertEqual(self.arw.active_round_count, 3)
+        self.assertEqual(self.arw.default_ljp, 0)
+
+    @patch("django.contrib.admin.ModelAdmin.message_user", new_callable=PropertyMock)
+    def test_approve_reg(self, model_admin):
+        approve_request = self.rf.post("admin:approve_registration")
+        approve_request.user = self.superuser
+        self.assertEqual(self.reg.status, "pending")
+        self.assertEqual(
+            SeasonPlayer.objects.filter(player__lichess_username="newplayer").count(), 0
+        )
+        self.arw.approve_reg(
+            approve_request,
+            modeladmin=model_admin,
+            send_confirm_email=False,
+            invite_to_slack=False,
+            season=self.season,
+            retroactive_byes=0,
+            late_join_points=0,
+        )
+        self.assertEqual(self.reg.status, "approved")
+        self.assertEqual(
+            SeasonPlayer.objects.filter(player__lichess_username="newplayer").count(), 1
+        )
 
 
 class UpdateBoardOrderWorkflowTestCase(TestCase):
