2850ba538833b2fd7806f4e1e8090b3a8bdc5b57
Lakin Wecker <lakin.wecker@gmail.com>
2025-07-26 08:02:04 -0600

    fix: Remove some files, update .dockerignore
    Co-authored-by: Lakin Wecker <lakin@gratch.taildb4d09.ts.net>

--- ubuntu-deps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get -y install python3.6 postgresql postgresql-client postgresql-server-dev-all mercurial libffi-dev libjpeg-dev
