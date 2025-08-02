f6cc83f51127c4011569d9f25e8c5c1710d5ce39
glbert-does <tachydromos@hotmail.com>
2025-08-03 00:09:53 +0200

    resolve migration merge conflict

--- heltour/tournament/migrations/0013_broadcasts.py
+++ heltour/tournament/migrations/0013_broadcasts.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("tournament", "0009_alter_playernotificationsetting"),
+        ("tournament", "0012_remove_registration_lichess_username"),
     ]
 
     operations = [
