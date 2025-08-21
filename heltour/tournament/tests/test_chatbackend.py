3925df9b9bd40b83153c01f7f263e9973ab5b239
glbert-does <tachydromos@hotmail.com>
2025-08-21 22:51:13 +0200

    add tests for team channel creation

--- heltour/tournament/tests/test_chatbackend.py
+++ heltour/tournament/tests/test_chatbackend.py
@@ -1,14 +1,16 @@
-from unittest.mock import patch
+from unittest.mock import call, patch
 
 from django.conf import settings
-from django.test import SimpleTestCase, override_settings
+from django.test import SimpleTestCase, TestCase, override_settings
 
 from heltour.tournament.chatbackend import (
+    SlackGroup,
     bold,
     channel_message,
     channellink,
     chatbackend,
     chatbackend_url,
+    create_team_channel,
     direct_user_message,
     dm_link,
     inlinecode,
@@ -20,7 +22,7 @@ from heltour.tournament.chatbackend import (
     userlink_ping,
     userlink_silent,
 )
-from heltour.tournament.tests.testutils import Shush
+from heltour.tournament.tests.testutils import createCommonLeagueData, Shush, get_team
 
 
 @override_settings(USE_CHATBACKEND="zulip")
@@ -160,8 +162,8 @@ class ZulipFormatTestCase(SimpleTestCase):
     #        }
     #    )
 
-    #@patch("zulip.Client")
-    #def test_multiple_user_message(self, client):
+    # @patch("zulip.Client")
+    # def test_multiple_user_message(self, client):
     #    client.return_value.register.return_value = {
     #        "result": "success",
     #        "max_topic_length": 100,
@@ -303,3 +305,119 @@ class SlackFormatTestCase(SimpleTestCase):
                 "attachments": [{"text": "testing direct messages to multiple users"}],
             },
         )
+
+
+class TeamChannelCreation(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        createCommonLeagueData()
+        cls.t = get_team("Team 1")
+
+    # TODO: find a good way to make the following test work without local imports
+    # @override_settings(USE_CHATBACKEND="zulip")
+    # @patch("reversion.create_revision")
+    # @patch("reversion.set_comment")
+    # @patch("zulip.Client")
+    # def test_zulip_create_team_channels(
+    #    self,
+    #    client,
+    #    r_comment,
+    #    r_revision,
+    # ):
+    #    zulip_success = {
+    #        "result": "success",
+    #        "stream_id": 125,
+    #    }
+
+    #    client.return_value.register.return_value = {
+    #        "result": "success",
+    #        "max_topic_length": 100,
+    #        "max_message_length": 1000,
+    #        "max_stream_name_length": 100,
+    #        "max_stream_description_length": 1000,
+    #    }
+    #    client.return_value.add_subscriptions.return_value = zulip_success
+    #    client.return_value.send_message.return_value = zulip_success
+    #    client.return_value.get_stream_id.return_value = zulip_success
+    #    with Shush():
+    #        create_team_channel(
+    #            team=self.t,
+    #            channel_name="channelname",
+    #            user_ids=["1", "2"],
+    #            topic="channel topic",
+    #            intro_message="welcome to your channel",
+    #        )
+    #    userids = [1, 2, settings.ZULIP_LISTENING_BOT, settings.ZULIP_HELTOUR_BOT]
+    #    client.return_value.add_subscriptions.assert_called_once_with(
+    #        streams=[
+    #            {
+    #                "name": "channelname",
+    #                "description": "channel topic",
+    #                "invite_only": True,
+    #                "history_public_to_subscribers": False,
+    #            }
+    #        ],
+    #        principals=userids,
+    #        invite_only=True,
+    #        history_public_to_subscribers=False,
+    #        can_add_subscribers_group={
+    #            "direct_members": userids,
+    #            "direct_subgroups": [],
+    #        },
+    #        can_remove_subscribers_group={
+    #            "direct_members": userids,
+    #            "direct_subgroups": [],
+    #        },
+    #        can_administer_channel_group={
+    #            "direct_members": userids,
+    #            "direct_subgroups": [],
+    #        },
+    #    )
+    #    client.return_value.send_message.assert_called_once_with(
+    #        {
+    #            "type": "channel",
+    #            "to": 125,
+    #            "content": "welcome to your channel",
+    #            "topic": "Welcome!",
+    #        }
+    #    )
+    #    client.return_value.get_stream_id.assert_called_once_with("channelname")
+    #    self.assertEqual("125", get_team("Team 1").slack_channel)
+
+    @override_settings(USE_CHATBACKEND="slack")
+    @patch(
+        "heltour.tournament.slackapi.create_group",
+        return_value=SlackGroup("id1", "channelname"),
+    )
+    @patch("heltour.tournament.slackapi.invite_to_group")
+    @patch("heltour.tournament.slackapi.set_group_topic")
+    @patch("heltour.tournament.slackapi.leave_group")
+    @patch("heltour.tournament.slackapi.send_message")
+    @patch("reversion.create_revision")
+    @patch("reversion.set_comment")
+    def test_slack_create_team_channels(
+        self,
+        r_comment,
+        r_revision,
+        send_message,
+        leave_group,
+        set_group_topic,
+        invite_to_group,
+        create_group,
+    ):
+        create_team_channel(
+            team=self.t,
+            channel_name="channelname",
+            user_ids=["1", "2"],
+            topic="channel topic",
+            intro_message="welcome to your channel",
+        )
+        create_group.assert_called_once_with("channelname")
+        invite_to_group.assert_has_calls(
+            [call("id1", ["1", "2"]), call("id1", settings.SLACK_LISTENING_BOT_ID)],
+            any_order=True,
+        )
+        set_group_topic.assert_called_once_with("id1", "channel topic")
+        leave_group.assert_called_once()
+        send_message.assert_called_once_with("#channelname", "welcome to your channel")
+        self.assertEqual("id1", get_team("Team 1").slack_channel)
