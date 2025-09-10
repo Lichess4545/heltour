fe573f726870921a683fc8049ef6771bc5f7da3b
glbert-does <tachydromos@hotmail.com>
2025-09-10 23:27:48 +0200

    more tests for notify.py

--- heltour/tournament/tests/test_notify.py
+++ heltour/tournament/tests/test_notify.py
@@ -3,14 +3,68 @@ from unittest.mock import patch
 from django.test import TestCase
 from django.utils import timezone
 
-from heltour.tournament.models import LeagueSetting, LonePlayerPairing
-from heltour.tournament.notify import notify_players_game_scheduled
+from heltour.tournament.models import LeagueChannel, LeagueSetting, LonePlayerPairing
+from heltour.tournament.notify import (
+    _message_multiple_users,
+    _message_user,
+    _send_notification,
+    notify_players_game_scheduled,
+)
 from heltour.tournament.tests.testutils import (
     createCommonLeagueData,
     get_league,
     get_player,
     get_round,
 )
+
+
+@patch("heltour.tournament.slackapi.send_message", autospec=True)
+class UnderscoreFunctions(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        createCommonLeagueData()
+        cls.l = get_league("team")
+        cls.l.enable_notifications = True
+        cls.l.save()
+        cls.lc = LeagueChannel.objects.create(
+            league=cls.l, type="mod", slack_channel="#test_mods"
+        )
+        cls.lc = LeagueChannel.objects.create(
+            league=cls.l, type="captains", slack_channel="#test_captains"
+        )
+
+    def test_send_notification(self, sm):
+        tm = "test message"
+        atm = "another test message"
+        result = _send_notification(notification_type="mod", league=self.l, text=tm)
+        self.assertEqual(result, None)
+        sm.assert_called_once_with("#test_mods", tm)
+        sm.reset_mock()
+        result_c = _send_notification(
+            notification_type="captains", league=self.l, text=atm
+        )
+        self.assertEqual(result_c, None)
+        sm.assert_called_once_with("#test_captains", atm)
+
+    def test_message_user(self, sm):
+        tm = "hello, how are you"
+        atm = "you want to play against @random_user?"
+        result = _message_user(league=self.l, username="glbert", text=tm)
+        self.assertEqual(result, None)
+        sm.assert_called_once_with("@glbert", text=tm)
+        sm.reset_mock()
+        result = _message_user(league=self.l, username="blah_$_blah", text=atm)
+        sm.assert_called_once_with("@blah_$_blah", text=atm)
+
+    def test_message_multiple_users(self, sm):
+        tm = "you people should schedule your games! #scheduling"
+        result = _message_multiple_users(
+            league=self.l,
+            usernames=["glbert", "test_user", "lakinwecker", "chesster"],
+            text=tm,
+        )
+        self.assertEqual(result, None)
+        sm.assert_called_once_with("@glbert+@test_user+@lakinwecker+@chesster", tm)
 
 
 class PairingNotificationsTestCase(TestCase):
